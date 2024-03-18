dadf7398f1a40ec316034a9bb39624888a5130b4
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-03-18 19:29:48 -0400

    Added Context

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,1 +1,29 @@
 /* TODO Add Theming to pass into components for awesome visuals*/
+
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  let theme = darkMode
+    ? {
+        background: "#121212",
+        color: "white",
+        outlineColor: "white",
+      }
+    : {
+        background: "white",
+        color: "black",
+        outlineColor: "black",
+      };
+
+  return (
+    <ThemeContext.Provider value={[darkMode, setDarkMode, theme]}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,2 +1,18 @@
 /* TODO Add any global parameters to pass into components as props*/
 /* See the "usecontext" lession for help */
+
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [userName, setUserName] = useState("Default Value");
+
+  return (
+    <UserContext.Provider value={[userName, setUserName]}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
