4dfc416e533683ad433f5d3bdcb3bf6079f50ff3
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-03-25 19:00:56 -0400

    Build created

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>This is my Portfolio - In Progress</h2>
+      <h3>John A. Isabella III</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
